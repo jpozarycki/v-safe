678aa2701f970fb8437a8f5c47843fca6268dd13
jpozarycki <jakub.pozarycki@gmail.com>
2025-06-16 16:31:19 +0200

    get passwords work

--- backend/src/main/java/com/jpozarycki/backend/password/PasswordGetController.java
+++ backend/src/main/java/com/jpozarycki/backend/password/PasswordGetController.java
@@ -1,6 +1,10 @@
 package com.jpozarycki.backend.password;
 
+import com.jpozarycki.backend.password.dto.GetPasswordsResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,4 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 class PasswordGetController {
 
     private final PasswordGetService passwordGetService;
+
+    @GetMapping(path = "/api/password")
+    ResponseEntity<GetPasswordsResponse> getPasswords(@RequestParam(name = "domain", required = false) String domain) {
+        var passwords = passwordGetService.getPasswords(domain);
+        return ResponseEntity.ok(new GetPasswordsResponse(passwords));
+    }
 }
--- backend/src/main/java/com/jpozarycki/backend/password/impl/PasswordGetServiceImpl.java
+++ backend/src/main/java/com/jpozarycki/backend/password/impl/PasswordGetServiceImpl.java
@@ -1,15 +1,30 @@
 package com.jpozarycki.backend.password.impl;
 
+import com.jpozarycki.backend.password.PasswordEntity;
 import com.jpozarycki.backend.password.PasswordGetService;
+import com.jpozarycki.backend.password.PasswordRepository;
 import com.jpozarycki.backend.password.dto.PasswordDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 class PasswordGetServiceImpl implements PasswordGetService {
+    private final PasswordRepository passwordRepository;
+
     @Override
-    public List<PasswordDto> getPasswords(final String domain) {
-        return List.of();
+    public List<PasswordDto> getPasswords(@Nullable final String domain) {
+        List<PasswordEntity> passwords;
+        if (domain == null) {
+            passwords = passwordRepository.findAll();
+        } else {
+            passwords = passwordRepository.findByDomain(domain);
+        }
+        return passwords.stream()
+                .map(password -> new PasswordDto(password.getId(), password.getDomain(), password.getPassword()))
+                .toList();
     }
 }
